5f76618ac80c906c850c241f105b1f483288ee0e
Amila Sikalo <asikalo2@etf.unsa.ba>
2018-12-03 11:48:01 +0100

    Kreirala instancu klase Grad da provjerim da li radi.

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -5,6 +5,9 @@ package ba.unsa.rpr.tutorijal7;
 
 public class Tutorijal {
     public static void main(String[] args) {
-
+        Double[] t = {1.0, 2.1, 3.2, 4.5, -2.1};
+        Grad n = new Grad("Sarajevo", 400000, t);
+        System.out.println(n.getNaziv());
+        System.out.println(n.getTemperature());
     }
 }
